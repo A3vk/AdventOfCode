f8631a542b356bf1b3815e3b3d01d0d93c32cb62
Adri van Kempen <adrivankempen@gmail.com>
2024-12-03 22:35:03 +0100

    [feat] AoC Day 3 - Part 2

--- AdventOfCode/2024/Day03/Solution.cs
+++ AdventOfCode/2024/Day03/Solution.cs
@@ -1,32 +1,94 @@
 using System.Text.RegularExpressions;
 using AdventOfCode.Core;
-using AdventOfCode.Core.Extensions;
 
 namespace AdventOfCode._2024.Day03;
 
 [ProblemName("Mull It Over")]
-public class Solution : ISolver
+public partial class Solution : ISolver
 {
     public string PartOne(string input)
     {
-        var expressions = GetMultiplicationExpressions(input);
+        var expressions = GetMultiplyExpressions(input);
         return expressions.Sum(exp => exp.X * exp.Y).ToString();
     }
 
     public string PartTwo(string input)
     {
-        throw new NotImplementedException();
+        var expressions = GetExpressions(input);
+
+        int result = 0;
+        bool isEnabled = true;
+        foreach (var expression in expressions)
+        {
+            if (isEnabled && expression.Token == Token.Multiply)
+            {
+                result += expression.X * expression.Y;
+                continue;
+            }
+            
+            isEnabled = expression.Token switch
+            {
+                Token.Do => true,
+                Token.DoNot => false,
+                _ => isEnabled
+            };
+        }
+        
+        return result.ToString();
     }
 
-    private List<MultiplicationExpression> GetMultiplicationExpressions(string input)
+    private List<Expression> GetMultiplyExpressions(string input)
     {
-        var matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)");
-        return matches.Select(match => new MultiplicationExpression { X = int.Parse(match.Groups[1].Value), Y = int.Parse(match.Groups[2].Value) }).ToList();
+        var expressions = GetExpressions(input);
+        return expressions.Where(x => x.Token == Token.Multiply).ToList();
     }
 
-    private struct MultiplicationExpression
+    private List<Expression> GetExpressions(string input)
     {
+        List<Expression> expressions = [];
+        
+        var matches = ExpressionRegex().Matches(input);
+        foreach (Match match in matches)
+        {
+            var token = ToToken(match.Groups[1].Value);
+            var expression = new Expression() { Token = token };
+            if (token == Token.Multiply)
+            {
+                expression = expression with
+                {
+                    X = int.Parse(match.Groups[3].Value), 
+                    Y = int.Parse(match.Groups[4].Value)
+                };
+            }
+            
+            expressions.Add(expression);
+        }
+        
+        return expressions;
+    }
+
+    private Token ToToken(string tokenString) => tokenString switch
+    {
+        "mul" => Token.Multiply,
+        "do" => Token.Do,
+        "don't" => Token.DoNot,
+        _ => throw new ArgumentOutOfRangeException(nameof(tokenString), tokenString, "The given token string is not supported!")
+    };
+
+    private struct Expression
+    {
+        public Token Token { get; init; }
         public int X { get; init; }
         public int Y { get; init; }
     }
+
+    private enum Token
+    {
+        Multiply,
+        Do,
+        DoNot
+    }
+
+    [GeneratedRegex(@"(mul|do|don't)\(((\d+),(\d+))?\)")]
+    private static partial Regex ExpressionRegex();
 }
